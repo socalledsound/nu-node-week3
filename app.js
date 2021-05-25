c326a838d8bca2b4420a57e09a47df3ffb2b6c8f
chris kubick <lrs6000@gmail.com>
2021-05-24 20:44:06 -0700

    sessions

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
@@ -36,11 +36,42 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}))
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+const auth = (req, res, next) => {
+  console.log(req.session)
+  if(!req.session.user){
+      const err = new Error('not a registered user')
+      err.status = 401
+      return next(err)
+  } else {
+    if(req.session.user == 'authenticated'){
+      return next()
+    } else {
+      const err = new Error('not authenticated')
+      err.status = 401
+      return next(err)
+    }
+  }
+}
+
+app.use(auth)
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
